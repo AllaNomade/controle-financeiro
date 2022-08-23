67d4d80e5b71c6446788eabdfde2b067adc5105f
Allan <allanpaiva17@gmail.com>
2022-08-23 18:13:41 -0300

    header and resume added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as Style from "./Header.styles"
+
+const Header = () => {
+  return (
+    <Style.Container>
+      <Style.Header>
+        <Style.Title>Controle Financeiro</Style.Title>
+      </Style.Header>
+    </Style.Container>
+  )
+};
+
+export default Header;

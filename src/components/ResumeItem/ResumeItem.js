7066fb7cfe16cdd343faf85cd6035e673068591f
Allan <allanpaiva17@gmail.com>
2022-08-24 12:19:05 -0300

    icons added

--- src/components/ResumeItem/ResumeItem.js
+++ src/components/ResumeItem/ResumeItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import * as Style from "./ResumeItem.styles";
 
-const ResumeItem = () => {
+const ResumeItem = ({ title, Icon, value }) => {
   return (
     <Style.Container>
       <Style.Header>
-        <Style.HeaderTitle>teste</Style.HeaderTitle>
+        <Style.HeaderTitle>{title}</Style.HeaderTitle>
+        <Icon />
       </Style.Header>
-      <Style.Total>1000</Style.Total>
+      <Style.Total>{value}</Style.Total>
     </Style.Container>
   )
 };
